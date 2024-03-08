32d9198a99233b34a696a8ee3c20b5128fd17f1c
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-03-09 01:21:46 +0530

    bit

---
+++ bitManupulation/bit.java
@@ -0,0 +1,34 @@
+package bitManupulation;
+
+import java.util.Scanner;
+
+public class bit {
+    public static int getIthBit(int num,int i){
+        int x=1<<i;
+        if ((num & x) == 0){
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+    public static int setIthBit(int num,int i){
+        int x=1<<i;
+        return num | x;
+    }
+    public static int clearIthBit(int num,int i){
+        int x=~(1<<i);
+        return num & x;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        System.out.print("Enter the index: ");
+        int i=sc.nextInt();
+        System.out.println(i+"th bit of "+num+" is "+getIthBit(num,i));
+        System.out.println("result of "+i+"th bit set is "+setIthBit(num,i));
+        System.out.println("result of "+i+"th bit clear is "+clearIthBit(num,i));
+    }
+}
